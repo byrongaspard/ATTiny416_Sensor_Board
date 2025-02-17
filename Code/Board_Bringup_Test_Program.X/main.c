0d0023562fb67f847564f62f22013784c42b6beb
byrongaspard <56890175+byrongaspard@users.noreply.github.com>
2025-02-17 14:33:18 -0500

    Bringup code updated to exercise SPI and UART interaces

--- Code/Board_Bringup_Test_Program.X/main.c
+++ Code/Board_Bringup_Test_Program.X/main.c
@@ -3,10 +3,24 @@
  * Author: byrongaspard
  *
  * Created on February 15, 2025, 3:23 PM
+ * 
+ * This test program exercises the interfacing hardware on the custom ATTiny416 
+ * development board. 
+ * It initializes an external serial communication interface to the FTDI230 chip.
+ * Additionally, it exercises the external W25X20L flash chip.
+ * 
+ * Connecting PA6 to ground will cause the ATTiny416 to dump the contents of
+ * the W25X20L flash to the UART interface. 
+ * Connecting PA7 to ground will cause the ATTiny416 to erase the W25X20L flash.
+ * If both PA6 and PA7 are left floating, the ATTiny416 will look for the beginning
+ * of uninitialized flash and begin recording data.
+ * 
+ * TODO: Separate code out into logical *.c and *.h files. 
+ * 
  */
 
 // Clock Speed, including the Peripheral Clock.
-// F_CPU must be defined before including the delay.h library for the delay calls to work
+// Trap for new players: F_CPU must be defined before including the delay.h library for the delay calls to work.
 #define F_CPU   3300000UL
                 
 #include <stdio.h>
@@ -15,6 +29,8 @@
 #include <util/delay.h>
 #include <avr/xmega.h>
 
+#define TRUE                (1)
+#define FALSE               (0)
 
 // General Bit Getting/Setting/Clearing utilities
 #define GET_BIT(p,n) ((p >> n) & 0x1)
@@ -24,21 +40,35 @@
 // USART Definitions
 #define USART_S             (16UL)      
 #define USART_REG_FRACT     (64UL)    
-#define BAUD_RATE           (9600UL)
+#define BAUD_RATE           (115200UL)
 #define BAUD_REG_VAL        ( ((float)USART_REG_FRACT * F_CPU)/((float)USART_S * BAUD_RATE) )
 
 // W25X20L Info
-#define W25X20L_MFR_ID  (0xEF)
-#define W25X20L_DEV_ID  (0x11)
-
+#define W25X20L_MFR_ID              (0xEF)
+#define W25X20L_DEV_ID              (0x11)
+#define W25X20L_START_ADDR          (0x000000)
+#define W25X20L_END_ADDR            (0x03FFFF)
+#define W25X20L_PAGE_BYTES          (256)
+#define W25X20L_STATUS_REG_WEN_BIT  (1UL)
+#define W25X20L_STATUS_REG_BUSY_BIT (0UL)
+#define W25X20L_NUM_PAGES           ((W25X20L_END_ADDR + 1UL) / W25X20L_PAGE_BYTES)
+#define W25X20L_CHIP_ERASE_TIME_MS  (2500UL)
 
 // W25X20L SPI Flash Commands
 #define W25X20L_READ_MFR_DEV_ID     (0x90)
+#define W25X20L_WRITE_ENABLE        (0x06)
+#define W25X20L_READ_STATUS_REG     (0x05)
+#define W25X20L_CHIP_ERASE          (0xC7)
+#define W25X20L_READ_DATA           (0x03) 
+#define W25X20L_PAGE_PROGRAM        (0x02)
 
 // SPI helper commands
 #define ASSERT_SELECT       (0)
 #define DEASSERT_SELECT     (1)
 
+// Global Variables
+volatile uint32_t cur_spi_write_addr = 0UL;
+volatile uint8_t spi_write_permitted = FALSE;
 
 // Function prototypes
 void initialize_clock(void);
@@ -46,11 +76,16 @@ void configure_alt_pinning(void);
 void configure_data_direction(void);
 void configure_uart(void);
 void configure_spi(void);
-
 void set_spi_cs(uint8_t val);
 uint8_t spi_transfer(uint8_t byte);
 void get_spi_device_id(void);
 
+void W25X20L_write_enable(void);
+void W25X20L_chip_erase(void);
+void W25X20L_find_uninitialized_memory(void);
+void W25X20L_write_data(uint8_t *data, uint8_t num_bytes);
+void W25X20L_dump_flash_to_uart(void);
+uint8_t W25X20L_get_status_reg(void);
 
 void send_uart_byte(uint8_t byte);
 void send_uart_string(char* str);
@@ -58,18 +93,54 @@ void send_uart_ascii_hex_byte(uint8_t byte);
 
 void main(void) 
 {
+    // Test recording array
+    uint8_t test_arr[] = {1, 2, 3, 4};
     
+    // Initialize everything
     initialize_clock();
     configure_alt_pinning();
     configure_data_direction();
     configure_uart();
     configure_spi();
 
+    // Check that the W25X20L is detected
     get_spi_device_id();
+
    
+    if(!GET_BIT(PORTA.IN, 6))
+    {
+        send_uart_string("PA6 is LOW. Dumping Flash\n");
+        W25X20L_dump_flash_to_uart();
+        send_uart_string("\n\nFlash dump complete! Reboot required.\n");
+        while(1);
+    }
+    
+    if(!GET_BIT(PORTA.IN, 7))
+    {
+        send_uart_string("PA7 is LOW. Erasing Flash. \n");
+        W25X20L_chip_erase();
+        send_uart_string("Reboot required. \n");
+        while(1);
+    }
+    
+    W25X20L_find_uninitialized_memory();
+    
     while(1)
     {
-        send_uart_string("Hello!\n");
+        if(GET_BIT(PORTA.IN, 6))
+        {
+            W25X20L_write_enable();
+            send_uart_string("PA6 is HIGH. Recording Data. \n");
+            W25X20L_write_data(test_arr, sizeof(test_arr)/sizeof(test_arr[0]));
+            test_arr[0]++;
+            
+            send_uart_string("\ncur_spi_write_addr = 0x");
+            send_uart_ascii_hex_byte(((cur_spi_write_addr) >> 16) & 0xFF);
+            send_uart_ascii_hex_byte(((cur_spi_write_addr) >> 8) & 0xFF);
+            send_uart_ascii_hex_byte(((cur_spi_write_addr) >> 0) & 0xFF);
+            send_uart_string("\n");
+        }
+        
         _delay_ms(1000);
     }
     return;
@@ -77,7 +148,7 @@ void main(void)
 
 void initialize_clock(void)
 {
-    
+    // NOP...
     return;
 }
 
@@ -111,6 +182,13 @@ void configure_data_direction(void)
     // LED Configuration
         CLR_BIT(PORTA.OUT, 3); // LED is on PA3. Initialize ON.
         SET_BIT(PORTA.DIR, 3); // LED is on PA3. Configure as output.
+        
+    // GPIO Configuration
+        CLR_BIT(PORTA.DIR, 7);      // GPIO input on PA7
+        SET_BIT(PORTA.PIN7CTRL, 3); // Enable pull-up resistor
+       
+        CLR_BIT(PORTA.DIR, 6);      // GPIO input on PA6
+        SET_BIT(PORTA.PIN6CTRL, 3); // Enable pull-up resistor
 
     return;
 }
@@ -169,8 +247,14 @@ void get_spi_device_id(void)
     uint8_t mfr_id = 0x00;
     uint8_t dev_id = 0x00;
     
+    //TODO: Replace this with the JEDEC ID call.
+    
+    // Wait until the chip is ready.
+    while(GET_BIT(W25X20L_get_status_reg(), W25X20L_STATUS_REG_BUSY_BIT)) _delay_us(1);
+    
     set_spi_cs(ASSERT_SELECT);
     
+    /* Send the command IAW W25X20L datasheet. */
     spi_transfer(W25X20L_READ_MFR_DEV_ID);
     spi_transfer(0x00);
     spi_transfer(0x00);
@@ -181,20 +265,214 @@ void get_spi_device_id(void)
     
     set_spi_cs(DEASSERT_SELECT);
     
+    // Verify the mfr and dev IDs are what is expected.
     if(mfr_id != W25X20L_MFR_ID || dev_id != W25X20L_DEV_ID)
     {
         send_uart_string("WARNING: detected illegal flash chip");
-        send_uart_string("mfr_id is ");
+        send_uart_string("mfr_id is 0x");
         send_uart_ascii_hex_byte(mfr_id);
         send_uart_string("\n");
 
-        send_uart_string("device_id is ");
+        send_uart_string("device_id is 0x");
         send_uart_ascii_hex_byte(dev_id);
         send_uart_string("\n");
+        // TODO: Add error handling here so we don't try to write if we're not 
+        // talking to the right chip.
     }
     else
     {
         send_uart_string("Detected W25X20L Flash Chip.\n");
+    }
+}
+
+uint8_t W25X20L_get_status_reg(void)
+{
+    uint8_t status_reg = 0x00;
+    
+    // Read the status register.
+    set_spi_cs(ASSERT_SELECT);
+    spi_transfer(W25X20L_READ_STATUS_REG);
+    status_reg = spi_transfer(0x00);
+    set_spi_cs(DEASSERT_SELECT);
+    
+    return status_reg;
+}
+
+void W25X20L_write_enable(void)
+{
+    // Wait until the chip is ready.
+    while(GET_BIT(W25X20L_get_status_reg(), W25X20L_STATUS_REG_BUSY_BIT)) _delay_us(1);
+    
+    // Send Write Enable command.
+    set_spi_cs(ASSERT_SELECT);
+    spi_transfer(W25X20L_WRITE_ENABLE);
+    set_spi_cs(DEASSERT_SELECT);
+    
+    if(!GET_BIT(W25X20L_get_status_reg(), W25X20L_STATUS_REG_WEN_BIT))
+    {
+        send_uart_string("ERROR: Write enable failed.\n");
+        spi_write_permitted = FALSE;
+    }
+    
+    return;
+}
+
+void W25X20L_chip_erase(void)
+{
+    // Wait until the chip is ready.
+    while(GET_BIT(W25X20L_get_status_reg(), W25X20L_STATUS_REG_BUSY_BIT)) _delay_us(1);
+    
+    // Send the erase command.
+    set_spi_cs(ASSERT_SELECT);
+    spi_transfer(W25X20L_CHIP_ERASE);
+    set_spi_cs(DEASSERT_SELECT);
+    
+    // Poll the status register until the erase is complete as indicated by the BUSY bit.
+     while(GET_BIT(W25X20L_get_status_reg(), W25X20L_STATUS_REG_BUSY_BIT))
+     {
+         _delay_ms(10);
+        send_uart_string("Erasing...\n");
+     }
+    
+    send_uart_string("W25X20L Erase Complete.\n");
+}
+
+/*
+ * This through iterates through flash until it finds 16 consecutive bytes of 0xFF.
+ * The memory address is then aligned to the nearest 16-byte boundary which is considered
+ * to be the start of uninitialized memory.
+ */
+void W25X20L_find_uninitialized_memory(void)
+{
+    uint8_t consec_uninit_bytes = 0;
+    uint32_t cur_addr = W25X20L_START_ADDR;
+    uint32_t start_addr = W25X20L_START_ADDR;
+    
+    // Wait until the chip is ready.
+    while(GET_BIT(W25X20L_get_status_reg(), W25X20L_STATUS_REG_BUSY_BIT)) _delay_us(1);
+    
+    // Send the Read Data command IAW W25X20L datasheet
+    // Start reading from the beginning of memory
+    set_spi_cs(ASSERT_SELECT);
+    spi_transfer(W25X20L_READ_DATA); // Send the read command.
+    spi_transfer((cur_addr >> 16) & 0xFF);  // Shift upper byte of address
+    spi_transfer((cur_addr >> 8) & 0xFF);   // Shift middle byte of address
+    spi_transfer((cur_addr >> 0) & 0xFF);   // Shift lower byte of address
+    
+    // Loop while there is still data to read
+    while(cur_addr <= W25X20L_END_ADDR)
+    {
+        if(spi_transfer(0x00) == 0xFF)
+        {
+            consec_uninit_bytes++;
+            
+            // If the start of uninitialized flash has been found, find the nearest
+            // 16-byte aligned address.
+            if(consec_uninit_bytes == 16)
+            {
+                start_addr = cur_addr & 0xFFFFFFF0;
+                
+                send_uart_string("Start of memory is at 0x");
+                send_uart_ascii_hex_byte(((start_addr) >> 16) & 0xFF);
+                send_uart_ascii_hex_byte(((start_addr) >> 8) & 0xFF);
+                send_uart_ascii_hex_byte(((start_addr) >> 0) & 0xFF);
+                send_uart_string("\n");
+                
+                cur_spi_write_addr = start_addr;
+                spi_write_permitted = TRUE;
+                
+                return;
+            }
+        }
+        else
+        {
+            consec_uninit_bytes = 0;
+        }
+        cur_addr ++;
+    }
+    
+    send_uart_string("Could not find start of memory.");
+    set_spi_cs(DEASSERT_SELECT);
+}
+
+void W25X20L_write_data(uint8_t *data, uint8_t num_bytes)
+{
+    
+    volatile uint32_t addr = cur_spi_write_addr;
+    
+    // Check that there is enough memory to write the data.
+    if((cur_spi_write_addr + num_bytes) > W25X20L_END_ADDR)
+    {
+        send_uart_string("Aborting W25X20L write... not enough memory left\n");
+        return;
+    }
+    // CHeck that the write does not cross page boundaries.
+    if(((cur_spi_write_addr % W25X20L_PAGE_BYTES) + num_bytes) > W25X20L_PAGE_BYTES)
+    {
+        // Feature: Make this function recursive to support multi-page writes.
+        send_uart_string("Aborting W25X20L write... the requested write crosses page boundaries. Not supported.\n");
+        return;
+    }
+    
+    /* Enable writes */
+    W25X20L_write_enable();
+    
+    if(spi_write_permitted == TRUE)
+    {
+        /* Wait until the chip is ready */
+        while(GET_BIT(W25X20L_get_status_reg(), W25X20L_STATUS_REG_BUSY_BIT)) _delay_us(1);
+
+        set_spi_cs(ASSERT_SELECT);
+        spi_transfer(W25X20L_PAGE_PROGRAM); // Send the program command.
+        spi_transfer((cur_spi_write_addr >> 16) & 0xFF);  // Shift upper byte of address
+        spi_transfer((cur_spi_write_addr >> 8) & 0xFF);   // Shift middle byte of address
+        spi_transfer((cur_spi_write_addr >> 0) & 0xFF);   // Shift lower byte of address
+        for(uint8_t i = 0; i < num_bytes; i++)
+        {
+            spi_transfer(data[i]);
+        }
+        set_spi_cs(DEASSERT_SELECT);
+
+        _delay_ms(10);
+
+        cur_spi_write_addr += num_bytes;
+    }
+    return;
+}
+
+void W25X20L_dump_flash_to_uart(void)
+{
+    uint32_t cur_addr = W25X20L_START_ADDR;
+    
+    /* Wait until the chip is ready */
+    while(GET_BIT(W25X20L_get_status_reg(), W25X20L_STATUS_REG_BUSY_BIT)) _delay_us(1);
+  
+    // Dump one page at a time
+    for(uint32_t cur_page = 0; cur_page < 4 /*W25X20L_NUM_PAGES*/; cur_page++)
+    {
+        cur_addr = W25X20L_START_ADDR + (cur_page * W25X20L_PAGE_BYTES);
+        
+        set_spi_cs(ASSERT_SELECT);
+        spi_transfer(W25X20L_READ_DATA); // Send the read command.
+        spi_transfer((cur_addr >> 16) & 0xFF);  // Shift upper byte of address
+        spi_transfer((cur_addr >> 8) & 0xFF);   // Shift middle byte of address
+        spi_transfer((cur_addr >> 0) & 0xFF);   // Shift lower byte of address
+        
+        for(uint32_t cur_page_byte = 0; cur_page_byte < W25X20L_PAGE_BYTES; cur_page_byte ++)
+        {
+            if (((cur_page_byte) % 16) == 0 && cur_page_byte < W25X20L_PAGE_BYTES)
+            {
+                send_uart_string("\n");
+                send_uart_string("0x");
+                send_uart_ascii_hex_byte(((cur_addr + cur_page_byte) >> 16) & 0xFF);
+                send_uart_ascii_hex_byte(((cur_addr + cur_page_byte) >> 8) & 0xFF);
+                send_uart_ascii_hex_byte(((cur_addr + cur_page_byte) >> 0) & 0xFF);
+                send_uart_string(": ");
+            }
+            send_uart_ascii_hex_byte(spi_transfer(0x00));
+        }
+        
+        set_spi_cs(DEASSERT_SELECT);
     }
 }
 
@@ -227,7 +505,6 @@ void send_uart_string(char* str)
 void send_uart_ascii_hex_byte(uint8_t byte)
 {
     uint8_t cur_nibble;
-    send_uart_string("0x");
     
     cur_nibble = ((byte >> 4) & 0xF);
     
